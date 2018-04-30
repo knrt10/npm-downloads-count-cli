0981fc735117fce3f397524ff12b80ec44cd4274
knrt10 <tripathi.kautilya@gmail.com>
2018-04-30 15:03:45 +0530

    Added more tests

--- api/count.js
+++ api/count.js
@@ -35,21 +35,23 @@ function countLastWeek (packageName) {
     colWidths: [30, 20]
   })
   const url = `https://api.npmjs.org/downloads/range/last-week/${packageName}`
-  request(url, (error, res, body, next) => {
-    if (error) {
-      return next(error)
-    }
-    const data = JSON.parse(body)
-    if (data.error) {
-      console.log(red, data.error)
-    } else {
-      const downloads = data.downloads
-      downloads.forEach(result => {
-        var final = [`${result.day}`, `${result.downloads}`]
-        table.push(final)
-      })
-      console.log(table.toString())
-    }
+  return new Promise((resolve, reject) => {
+    resolve(request(url, (error, res, body, next) => {
+      if (error) {
+        return next(error)
+      }
+      const data = JSON.parse(body)
+      if (data.error) {
+        console.log(red, data.error)
+      } else {
+        const downloads = data.downloads
+        downloads.forEach(result => {
+          var final = [`${result.day}`, `${result.downloads}`]
+          table.push(final)
+        })
+        console.log(table.toString())
+      }
+    }))
   })
 }
 
@@ -64,21 +66,23 @@ function countLastMonth (packageName) {
     colWidths: [30, 20]
   })
   const url = `https://api.npmjs.org/downloads/range/last-month/${packageName}`
-  request(url, (error, res, body, next) => {
-    if (error) {
-      return next(error)
-    }
-    const data = JSON.parse(body)
-    if (data.error) {
-      console.log(red, data.error)
-    } else {
-      const downloads = data.downloads
-      downloads.forEach(result => {
-        var final = [`${result.day}`, `${result.downloads}`]
-        table.push(final)
-      })
-      console.log(table.toString())
-    }
+  return new Promise((resolve, reject) => {
+    resolve(request(url, (error, res, body, next) => {
+      if (error) {
+        return next(error)
+      }
+      const data = JSON.parse(body)
+      if (data.error) {
+        console.log(red, data.error)
+      } else {
+        const downloads = data.downloads
+        downloads.forEach(result => {
+          var final = [`${result.day}`, `${result.downloads}`]
+          table.push(final)
+        })
+        console.log(table.toString())
+      }
+    }))
   })
 }
 
@@ -89,16 +93,18 @@ function countLastMonth (packageName) {
 
 function countDay (date, packageName) {
   const url = `https://api.npmjs.org/downloads/point/${date}/${packageName}`
-  request(url, (error, res, body, next) => {
-    if (error) {
-      return next(error)
-    }
-    const data = JSON.parse(body)
-    if (data.error) {
-      console.log(red, data.error)
-    } else {
-      console.log(`\x1b[35m ${packageName} \x1b[0m got \x1b[36m ${data.downloads} \x1b[0m downloads on \x1b[36m ${data.end} \x1b[0m`)
-    }
+  return new Promise((resolve, reject) => {
+    resolve(request(url, (error, res, body, next) => {
+      if (error) {
+        return next(error)
+      }
+      const data = JSON.parse(body)
+      if (data.error) {
+        console.log(red, data.error)
+      } else {
+        console.log(`\x1b[35m ${packageName} \x1b[0m got \x1b[36m ${data.downloads} \x1b[0m downloads on \x1b[36m ${data.end} \x1b[0m`)
+      }
+    }))
   })
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -3,9 +3,30 @@ const api = require('../api/count')
 describe('get count', () => {
   it('check for last day', () => {
     const packageName = 'cloudinary-cli-tool'
-    const countLastDay = api.countLastDay(packageName)
-    Promise.all([countLastDay]).then(val => {
-      val[0].callback(packageName)
+    return api.countLastDay(packageName).then(data => {
+      expect(data.callback).toBeDefined()
+    })
+  })
+
+  it('check for last week', () => {
+    const packageName = 'cloudinary-cli-tool'
+    return api.countLastWeek(packageName).then(data => {
+      expect(data.callback).toBeDefined()
+    })
+  })
+
+  it('check for last month', () => {
+    const packageName = 'cloudinary-cli-tool'
+    return api.countLastMonth(packageName).then(data => {
+      expect(data.callback).toBeDefined()
+    })
+  })
+
+  it('check for specific date', () => {
+    const packageName = 'cloudinary-cli-tool'
+    const date = '2018-12-12'
+    return api.countDay(date, packageName).then(data => {
+      expect(data.callback).toBeDefined()
     })
   })
 })
